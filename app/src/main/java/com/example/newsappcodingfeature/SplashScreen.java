3ce03a24378e24526ab7491e868879e2c11e625f
Admin <maxmudovabdullo97@gmail.com>
2020-05-26 17:19:55 +0500

    News App Coding Feature

---
+++ app/src/main/java/com/example/newsappcodingfeature/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.newsappcodingfeature;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                  Thread.sleep(3000);
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });thread.start();
+    }
+}
